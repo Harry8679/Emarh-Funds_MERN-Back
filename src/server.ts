72e835eacf6421fd41635b7f88eb3f0a73daced4
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-22 21:23:48 +0200

    First running server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-// src/index.ts
-const greet = (name: string) => {
-    console.log(`Bonjour, ${name}!`);
-};
+import express from 'express';
+import dotenv from 'dotenv';
 
-greet("le monde");
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 5050;
+
+app.listen(port, () => console.log(`Server running at ${port}`));
