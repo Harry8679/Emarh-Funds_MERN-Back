1da7d57116f8fcfadcd8eca00d4ebedf820109a2
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-28 18:05:10 +0100

    register controller 1

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,18 @@
 import express from 'express';
+import UserModel from '../models/user.model';
 
 const router = express.Router();
+
+router.post('/register', async (req, res) => {
+    try {
+        // Check if the user has already registered
+        const user = await UserModel.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+});
 
 export default router;
