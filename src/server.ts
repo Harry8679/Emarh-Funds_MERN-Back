92f4c09b1fedc22aee643a963944e95533897222
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-28 17:46:16 +0100

    User Routes Middleware

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectMongoDB from './config/dbConnect';
+import userRoute from './routes/user.route';
 
 dotenv.config();
 
 connectMongoDB();
-// console.log('object')
+
 
 const app = express();
+
+app.use('/api/v1', userRoute);
 
 const port = process.env.PORT || 5060;
 
