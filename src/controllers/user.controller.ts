08f69ef3dfcf4a6867c8f57dedbfb1a7fd12775c
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-29 14:49:46 +0100

    JSON Web Token library installed

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,6 +3,7 @@ import UserModel from '../models/user.model';
 import express, { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 
+/************* Register ********************/
 export const register = async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -22,3 +23,25 @@ export const register = async (req: Request, res: Response) => {
         res.status(500).json({ message: 'An unexpected error occurred' });
     }
 }
+
+/************* Login ********************/
+export const login = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+        return;
+    }
+
+    const { email, password, name } = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await UserModel.create({ email, password: hashedPassword, name });
+
+        res.status(201).json({ message: 'User registered successfully', user });
+    } catch (err) {
+        console.error('Error occurred:', err);
+        res.status(500).json({ message: 'An unexpected error occurred' });
+    }
+}
+
